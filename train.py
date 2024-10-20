b6af1329f4e97afc769da6470d34f27fcca80d69
Tetsui Ohkubo <peryaudo@gmail.com>
2024-10-20 16:10:08 -0700

    Add validation steps

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from datasets import load_dataset
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
 
 from model import ResNetModel
 
@@ -27,16 +28,17 @@ class DatasetWrapper(Dataset):
 if __name__ == "__main__":
     model = ResNetModel().to("cuda")
     loss_func = nn.CrossEntropyLoss()
-    # outputs = model(torch.randn(16, 3, 32, 32))
     hf_dataset = load_dataset("uoft-cs/cifar10")
 
     train_dataset = DatasetWrapper(hf_dataset["train"])
-    train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=1)
+    train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=1)
+    val_dataset = DatasetWrapper(hf_dataset["test"])
+    val_dataloader = DataLoader(val_dataset, batch_size=512, shuffle=False, num_workers=1)
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 
     for epoch in range(10):
-        for step, (inputs, label) in enumerate(train_dataloader):
+        for step, (inputs, label) in enumerate(tqdm(train_dataloader)):
             optimizer.zero_grad()
             inputs = inputs.to("cuda")
             label = label.to("cuda")
@@ -45,4 +47,24 @@ if __name__ == "__main__":
             loss = loss_func(outputs, label)
             loss.backward()
             optimizer.step()
-            print(f"epoch: {epoch} step: {step} loss: {loss.detach().item()}")
+        print(f"epoch: {epoch} loss: {loss.detach().item()}")
+
+        val_loss = 0.0
+        correct_count = 0
+        val_count = 0
+        for step, (inputs, label) in enumerate(tqdm(val_dataloader)):
+            with torch.no_grad():
+                inputs = inputs.to("cuda")
+                label = label.to("cuda")
+                outputs = model(inputs)
+                loss = loss_func(outputs, label)
+                val_loss += loss.detach().item() * inputs.size(0)
+                val_count += inputs.size(0)
+
+                pred = torch.argmax(outputs, dim=-1)
+
+                correct_count += torch.sum((pred == label).long()).detach().item()
+        
+        accuracy = correct_count / val_count
+        val_loss /= val_count
+        print(f"accuracy: {accuracy} val_loss: {val_loss}")
