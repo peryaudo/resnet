75948ff6890b73747e4dcf81930b799580e798fc
Tetsui Ohkubo <peryaudo@gmail.com>
2024-10-22 00:27:43 -0700

    Disable bias for basic block

--- basic_model.py
+++ basic_model.py
@@ -5,18 +5,17 @@ class BasicResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
 
         self.residual_downsample = None
         if stride != 1 or in_channels != out_channels:
             self.residual_downsample = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(out_channels))
-
     
     def forward(self, x):
         if self.residual_downsample:
